6314f9f99567981388168dc5202fdc8e76726f07
rootart <dijakroot@gmail.com>
2013-09-13 16:01:30 +0300

    Automatic evaluations create on technology adding

--- src/seva/apps/technologies/models.py
+++ src/seva/apps/technologies/models.py
@@ -1,3 +1,6 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -41,3 +44,13 @@ class KeyMetrics(models.Model):
 
     def __unicode__(self):
         return self.technology.title
+
+
+@receiver(post_save, sender=Technology)
+def create_profile_for_user(sender, instance, created, **kwargs):
+    """
+    Creates selfevaluations
+    """
+    from evaluations.models import SelfEvaluation
+    for user in User.objects.all():
+        evl, created = SelfEvaluation.objects.get_or_create(technology=instance, user=user)
