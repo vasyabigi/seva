938ad6df48eca7b069b436c706fd89b44ead344b
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-10-10 17:20:08 +0300

    Added first grunt task.

--- src/seva/static/Gruntfile.js
+++ src/seva/static/Gruntfile.js
@@ -1,11 +1,23 @@
+'use strict';
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'js/app/**/*.js']
+    }
+
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['jshint']);
 
 };
