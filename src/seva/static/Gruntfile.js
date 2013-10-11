1567fe32ca97003a89d7ac80b7bc79c1a46dff07
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-10-11 13:21:45 +0300

    Added concat and uglify plugins.

--- src/seva/static/Gruntfile.js
+++ src/seva/static/Gruntfile.js
@@ -11,13 +11,42 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       },
       all: ['Gruntfile.js', 'js/app/**/*.js']
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/app/**/*.js'],
+        dest: 'js/dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! PANDA\'S <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('build', [
+    'jshint',
+    'concat',
+    'uglify'
+  ]);
 
 };
